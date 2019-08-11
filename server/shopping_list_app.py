a83245aecabbc2c2b41a07feae1eef17142c62c2
Chris <chrismjboyle@gmail.com>
2019-08-11 10:29:07 +0100

    Fleshed out main body of shopping app

--- server/shopping_list_app.py
+++ server/shopping_list_app.py
@@ -7,6 +7,19 @@ the aisles that will be needed in the shop.
 '''
 
 import json
+from database import Database
+
+database = Database('shopping_lists.json')
+recipes = Database('recipes.json')
+
+
+def get_sorted_list(shopping_list):
+    return []
+
+
+def get_shopping_list(menu):
+    return []
+
 
 def shopping_process(data):
     '''
@@ -15,8 +28,39 @@ def shopping_process(data):
     will then do the requested processing, and return the object.
     '''
 
-    # TODO this
-    reply = json.dumps({'reply': 'no', 'menukey': data['menukey']})
-    print(reply)
+    server_data = database[data['menukey']]
+    extra = dict()
+    extra['reply'] = 'ok'
+
+    # data rec'd should overwrite anything in server
+    for key in ['menu', 'list', 'sorted']:
+        if key in data:
+            server_data[key] = data[key]
+
+    if 'looking-for' in data:
+        wants = data['looking_for']
+    else:
+        wants = 'nothing'
+
+    if wants == 'menu' and not server_data['menu']:
+        server_data['menu'] = []
+    if wants == 'list':
+        if not server_data['menu']:
+            extra['reply'] = 'not ok'
+            extra['alert'] = 'Need to set a menu before a list can be made.'
+
+        server_data['list'] = get_shopping_list(server_data['menu'])
+    elif wants == 'sorted':
+        if not server_data['list']:
+            if not server_data['menu']:
+                extra['reply'] = 'not ok'
+                extra['alert'] = 'Need a menu before a list can be made, '
+                extra['alert'] += 'and need a list before it can be sorted.'
+            server_data['list'] = get_shopping_list(server_data['menu'])
+        server_data['sorted'] = get_sorted_list(server_data['list'])
+
+    reply = json.dumps({'menukey': data['menukey'], 'looking_for': wants, **extra, **server_data})
+
+    database[data['menukey']] = server_data
 
     return reply
